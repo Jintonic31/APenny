2841bbb721f3a5dc57ede9ac634ca2de2d10ad28
as <as@DESKTOP-VU2TISE>
2024-01-25 00:39:58 +0900

    [jintonic31] 프로젝트 최초 생성

---
+++ APenny/src/main/java/cafe/appeny/APennyApplication.java
@@ -0,0 +1,13 @@
+package cafe.appeny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APennyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APennyApplication.class, args);
+	}
+
+}
